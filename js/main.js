51056bdca0620133a2e6f2a8c5d326a71a0455c6
ziad2001kamal <124497442+ziad2001kamal@users.noreply.github.com>
2023-04-28 15:25:39 +0300

    uplod data

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// document.write("<h2>ziad kamal alnumilat</h2>");
+// document.querySelector("p").style.fontSize = '15px' ;
+// console.log("hello wortld");
+// console.log("ziad kamal");
+// console.error("Error");
+// console.table(['ali','ziad','ahmed']);
+let text="hello my name is ziad but slah is a donkey"
+console.log( text.charAt(19)  );
